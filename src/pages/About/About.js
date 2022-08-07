13258575270752a9b8f57d9b2a30359d68486e35
LisaVC <lisaviju@gmail.com>
2022-08-07 22:17:43 +0530

    added home and navbar

---
+++ src/pages/About/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import './About.css'
+
+const About = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className='text-color-white content'>
+                <p className='h4'>Full Stack Web Developer<br />Mic Lover!</p>
+                <p className='para'>
+                    Hey there! I'm Lisa VC, a full stack web developer
+                </p>
+                <p className='heading'>Skills</p>
+                <p className='heading'>Achievements</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/AllProjects/AllProjects.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+
+const AllProjects = () => {
+    return (
+        <div className='text-color-white content'>
+            <Navbar />
+            <p className='heading'>Projects</p>
+
+        </div>
+
+    )
+}
+
+export default AllProjects
